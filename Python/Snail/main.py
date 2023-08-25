d3d271f6bac4f1756ea6f211d22b79a08fe35011
Sam Douglas <44397135+Sam-Douglas@users.noreply.github.com>
2023-08-25 13:45:24 +0100

    Snail sort logic working

--- Python/Snail/main.py
+++ Python/Snail/main.py
@@ -2,9 +2,13 @@ from snail_sort_module.snail_sort import snail_sort
 
 
 def main():
-    array = [[1, 2, 3],
-             [8, 9, 4],
-             [7, 6, 5]]
+    #array = [[1, 2, 3],
+    #         [8, 9, 4],
+    #         [7, 6, 5]]
+    array = [[1, 2, 3, 4, 5, 6],
+             [16, 17, 18, 19, 20, 7],
+             [15, 24, 23, 22, 21, 8],
+             [14, 13, 12, 11, 10, 9]]
     snail_sort(array)
 
 
--- Python/Snail/snail_sort_module/snail_sort.py
+++ Python/Snail/snail_sort_module/snail_sort.py
@@ -4,23 +4,37 @@ import copy
 def snail_sort(snail_map: List[int]):
     result = []
     print(snail_map)
-
-    for item in snail_map[0]:
-        result.append(item)
-    del snail_map[0]
-
-    for i, item in enumerate(copy.deepcopy(snail_map)):
-        result.append(item[-1])
-        del snail_map[i][-1]
-    
-    for item in reversed(snail_map[-1]):
-        result.append(item)
-    del snail_map[-1]
-
-    for i, item in reversed(list(enumerate(copy.deepcopy(snail_map)))):
-        result.append(item[-1])
-        del snail_map[i][-1]
-
+    number_of_values = sum(len(x) for x in snail_map)
+    snail_functions = (__iterate_right, __iterate_down, __iterate_left, __iterate_up)
+    while len(snail_map) != 0:
+        for function in snail_functions:
+            snail_map, result = function(snail_map, result)
+            if len(snail_map) == 0:
+                break
     print(result)
     print(snail_map)
     pass
+
+def __iterate_right(snail_map, result):
+    for item in snail_map[0]:
+        result.append(item)
+    del snail_map[0]
+    return snail_map, result
+
+def __iterate_down(snail_map, result):
+    for i, item in enumerate(copy.deepcopy(snail_map)):
+        result.append(item[-1])
+        del snail_map[i][-1]
+    return snail_map, result
+
+def __iterate_left(snail_map, result):
+    for item in reversed(snail_map[-1]):
+        result.append(item)
+    del snail_map[-1]
+    return snail_map, result
+
+def __iterate_up(snail_map, result):
+    for i, item in reversed(list(enumerate(copy.deepcopy(snail_map)))):
+        result.append(item[0])
+        del snail_map[i][0]
+    return snail_map, result
